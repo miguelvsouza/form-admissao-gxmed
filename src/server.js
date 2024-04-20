35172d1bd7b9d15c4906b3e11971d1fbbca1fb37
miguelvsouza <miguelvsouza77@gmail.com>
2024-04-19 22:33:29 -0300

    rota-get-version

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
-const express = require('express')
 require('dotenv').config()
+const packageJson = require('../package.json')
+const express = require('express')
 const app = express()
 
 app.use(express.static('../public'))
@@ -8,4 +9,13 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`O servidor está rodando na porta ${PORT}.`)
+})
+
+app.get('/version', (req, res) => {
+    res.status(200).send(
+        {
+            "version": packageJson.version,
+            "name": packageJson.name
+        }
+    )
 })
